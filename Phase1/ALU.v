ea018dbed803c19ca7b87110f7bd490d3c93071f
Lorenzo DeMarni <88600983+LorenzoDeMarni@users.noreply.github.com>
2026-02-03 18:55:15 -0500

    Need to add ALU operations for logical, arithmetic, and bitwise functions

--- Phase1/ALU.v
+++ Phase1/ALU.v
@@ -10,6 +10,17 @@ module ALU(input wire [7:0] A, B, input wire [3:0] op, output reg[7:0] result);
 		case(op)
 			0	:	result = or_result;
 			1	:	result = and_result;
+			2	:	result = not_result;
+			3	:	result = add_result;
+			4	:	result = sub_result;
+			5	:	result = neg_result;
+			6	:	result = mul_result;
+			7	:	result = div_result;
+			8	:	result = shift_left_result;
+			9	:	result = shift_right_result;
+			10	:	result = shift_right_arithmetic_result;
+			11	:	result = rotate_left_result;
+			12	:	result = rotate_right_result;
 			// ... 
 			default: result = and_result;
 		endcase
