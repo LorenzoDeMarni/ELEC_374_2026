beb917f28535bad8e6e97e2aeceaa7fd900d63c1
Ethan Toste <22xgs1@queensu.ca>
2026-02-08 18:15:01 -0500

    Final Updates

--- Phase1/ALU.v
+++ Phase1/ALU.v
@@ -1,7 +1,12 @@
-module ALU(input wire [31:0] A, B, input wire [3:0] op, output reg[31:0] result);
+module ALU(input wire [31:0] A, B, input wire [3:0] op, output reg[63:0] result);
 	
 	
-	wire [31:0] and_result, or_result;
+	wire [31:0] and_result, or_result, not_result, neg_result;
+    wire [31:0] add_result, sub_result;
+    wire [31:0] shift_left_result, shift_right_result, shift_right_arithmetic_result;
+    wire [31:0] rotate_left_result, rotate_right_result;
+    wire [63:0] mul_result;
+    wire [31:0] div_quotient, div_remainder;
 	
 	and_or and_instance(A, B, 1, and_result);
 	and_or or_instance(A, B, 0, or_result);
@@ -11,20 +16,30 @@ module ALU(input wire [31:0] A, B, input wire [3:0] op, output reg[31:0] result)
 
 	assign shift_left_result = A << B[4:0];
 	assign shift_right_result = A >> B[4:0];
+	assign shift_right_arithmetic_result = $signed(A) >>> B[4:0];
 	assign rotate_left_result = (A << B[4:0]) | (A >> (32 - B[4:0]));
+	assign rotate_right_result = (A >> B[4:0]) | (A << (32 - B[4:0]));
 
-	
+	adder add_instance(A, B, add_result);
+    
+    wire [31:0] neg_B = -B;
+    adder sub_instance(A, neg_B, sub_result);
+    
+    booth_multiplier mul_instance(A, B, mul_result);
+    
+    NRDivider div_instance(A, B, div_quotient, div_remainder);
+
 	
 	always @(*) begin
 		case(op)
 			0	:	result = or_result;
 			1	:	result = and_result;
 			2	:	result = not_result;
-			3	:	result = add_result;
-			4	:	result = sub_result;
+			3	:	result = {32'd0, add_result};
+			4	:	result = {32'd0, sub_result};
 			5	:	result = neg_result;
 			6	:	result = mul_result;
-			7	:	result = div_result;
+			7	:	result = {div_remainder, div_quotient};
 			8	:	result = shift_left_result;
 			9	:	result = shift_right_result;
 			10	:	result = shift_right_arithmetic_result;
